98fe163e39105f164ad491d321db3791fe90c156
LeandroPerez <leaperez435@gmail.com>
2025-06-04 20:08:33 -0300

    Commit cinco L

---
+++ src/models/index.js
@@ -0,0 +1,72 @@
+const sequelize = require('../config/database');
+const User = require('./user.model');
+const Bovino = require('./bovino.model');
+const Raza = require('./raza.model');
+const Mensaje = require('./mensaje.model');
+const Resena = require('./resena.model');
+const CaracteristicaGenetica = require('./caracteristicaGenetica.model');
+const ValorCaracteristica = require('./valorCaracteristica.model');
+const HistorialReproduccion = require('./historialReproduccion.model');
+
+
+// --- Asociaciones --- //
+User.hasMany(Bovino, { foreignKey: 'vendedorId', as: 'bovinosEnVenta' });
+Bovino.belongsTo(User, { foreignKey: 'vendedorId', as: 'propietario' });
+
+Raza.hasMany(Bovino, { foreignKey: 'razaId' });
+Bovino.belongsTo(Raza, { foreignKey: 'razaId', as: 'raza' });
+
+User.hasMany(Mensaje, { foreignKey: 'emisorId', as: 'mensajesEnviados' });
+User.hasMany(Mensaje, { foreignKey: 'receptorId', as: 'mensajesRecibidos' });
+Mensaje.belongsTo(User, { foreignKey: 'emisorId', as: 'emisor' });
+Mensaje.belongsTo(User, { foreignKey: 'receptorId', as: 'receptor' });
+
+User.hasMany(Resena, { foreignKey: 'compradorId', as: 'resenasRealizadas' });
+Resena.belongsTo(User, { foreignKey: 'compradorId', as: 'compradorResena' });
+User.hasMany(Resena, { foreignKey: 'vendedorId', as: 'resenasRecibidas' });
+Resena.belongsTo(User, { foreignKey: 'vendedorId', as: 'vendedorResena' });
+
+Bovino.belongsToMany(CaracteristicaGenetica, {
+  through: ValorCaracteristica,
+  foreignKey: 'bovinoId',
+  otherKey: 'caracteristicaGeneticaId',
+  as: 'caracteristicasGeneticas'
+});
+CaracteristicaGenetica.belongsToMany(Bovino, {
+  through: ValorCaracteristica,
+  foreignKey: 'caracteristicaGeneticaId',
+  otherKey: 'bovinoId',
+  as: 'bovinosAsociados'
+});
+ValorCaracteristica.belongsTo(Bovino, { foreignKey: 'bovinoId' });
+ValorCaracteristica.belongsTo(CaracteristicaGenetica, { foreignKey: 'caracteristicaGeneticaId' });
+
+Bovino.hasMany(HistorialReproduccion, { foreignKey: 'bovinoId', as: 'historialReproductivo' });
+HistorialReproduccion.belongsTo(Bovino, { foreignKey: 'bovinoId', as: 'bovino' });
+
+
+const syncDatabase = async () => {
+  try {
+    await sequelize.authenticate(); // 
+    console.log('Database connection OK!'); // 
+    await sequelize.sync({ alter: true }); // alter: true intenta hacer cambios sin perder datos (para desarrollo) 
+    console.log('Base de datos sincronizada correctamente.');
+  } 
+  catch (error) {
+    console.error('Error al conectar o sincronizar la base de datos:', error.message); // 
+    process.exit(1); // Salir del proceso si no se puede conectar a la DB
+  }
+};
+
+module.exports = {
+  sequelize,
+  User,
+  Bovino,
+  Raza,
+  Mensaje,
+  Resena,
+  CaracteristicaGenetica,
+  ValorCaracteristica,
+  HistorialReproduccion,
+  syncDatabase,
+};
